4749ecd2fe429429bf9d044ebcbc86ae92229ac6
VanshAgarwal23 <vanshagrawalitarsi23@gmail.com>
2026-09-14 20:56:13 +0530

    Verify security system simulation

--- Smart_Home_Security.srcs/sim_1/new/smart_security_tb.v
+++ Smart_Home_Security.srcs/sim_1/new/smart_security_tb.v
@@ -1,26 +1,62 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 14.09.2026 20:46:44
-// Design Name: 
-// Module Name: smart_security_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+module smart_security_tb;
 
-module smart_security_tb(
+reg D;
+reg W;
+reg M;
 
-    );
+wire Door_Alarm;
+wire Window_Alarm;
+wire Motion_Alarm;
+wire Master_Alarm;
+
+smart_security uut (
+    .D(D),
+    .W(W),
+    .M(M),
+    .Door_Alarm(Door_Alarm),
+    .Window_Alarm(Window_Alarm),
+    .Motion_Alarm(Motion_Alarm),
+    .Master_Alarm(Master_Alarm)
+);
+
+initial begin
+
+    // 000 - No sensor
+    D = 0; W = 0; M = 0;
+    #10;
+
+    // 001 - Motion
+    D = 0; W = 0; M = 1;
+    #10;
+
+    // 010 - Window
+    D = 0; W = 1; M = 0;
+    #10;
+
+    // 011 - Window + Motion
+    D = 0; W = 1; M = 1;
+    #10;
+
+    // 100 - Door
+    D = 1; W = 0; M = 0;
+    #10;
+
+    // 101 - Door + Motion
+    D = 1; W = 0; M = 1;
+    #10;
+
+    // 110 - Door + Window
+    D = 1; W = 1; M = 0;
+    #10;
+
+    // 111 - All sensors
+    D = 1; W = 1; M = 1;
+    #10;
+
+    $finish;
+
+end
+
 endmodule
